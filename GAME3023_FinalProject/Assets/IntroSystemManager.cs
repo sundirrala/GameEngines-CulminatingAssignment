683f62ba18baf4085d1ba5b944499c234bd669c9
Lukatiel <54953809+Lukatiel@users.noreply.github.com>
2022-10-09 00:25:42 -0400

    Added Intro Scene
    Added all buttons and text boxes for the intro scene

---
+++ GAME3023_FinalProject/Assets/IntroSystemManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroSystemManager : MonoBehaviour
+{
+    [SerializeField]
+    Button NewGameButton, ContinueButton, CreditsButton, BackButton, ExitButton;
+
+    [SerializeField]
+    GameObject CreditsBoxes;
+
+    private void Start()
+    {
+        BackButton.gameObject.SetActive(false);
+        CreditsBoxes.SetActive(false);
+    }
+
+    //For New Game : To add more later
+    public void NewGame()
+    {
+        SceneManager.LoadScene("OverworldScene");
+
+        //Clear saved data if any?
+    }
+
+    //For Continue : add more later
+    public void Continue()
+    {
+        SceneManager.LoadScene("OverworldScene");
+
+        //Load saved data
+    }
+
+    //Credits
+    public void Credits()
+    {
+        //Make Credits UI visable
+        BackButton.gameObject.SetActive(true);
+        CreditsBoxes.SetActive(true);
+
+        //Make Other UI invisable
+        NewGameButton.gameObject.SetActive(false);
+        ContinueButton.gameObject.SetActive(false);
+        ExitButton.gameObject.SetActive(false);
+        CreditsButton.gameObject.SetActive(false);
+    }
+
+    public void Back()
+    {
+        //Make Credit UI invisable
+        BackButton.gameObject.SetActive(false);
+        CreditsBoxes.SetActive(false);
+
+        //Make Other UI visable again
+        NewGameButton.gameObject.SetActive(true);
+        ContinueButton.gameObject.SetActive(true);
+        ExitButton.gameObject.SetActive(true);
+        CreditsButton.gameObject.SetActive(true);
+    }
+
+    //For Exit Button
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        Debug.Log("Ending Game...");
+    }
+}
